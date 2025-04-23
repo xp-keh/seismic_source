a8abaf39293655550e16419512dd80f62320afa0
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-04-24 00:29:57 +0700

    chore: changed stations #2

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import threading
 from producer.kafka import SeismicSeedLinkClient
 from producer.station_metadata import STATIONS
 from config.utils import get_env_value
+from config.logger import Logger
+
+logger = Logger().setup_logger(service_name='seismic_producer')
 
 app = FastAPI()
 
@@ -10,7 +13,7 @@ app = FastAPI()
 def health():
     return {"status": "ok"}
 
-print("Stations to be used:", STATIONS)
+logger.info("Stations to be used:", STATIONS)
 
 def stream_data_to_kafka():
     kafka_broker = get_env_value('KAFKA_BROKER')
@@ -27,5 +30,8 @@ def stream_data_to_kafka():
     seismic_client.create_instance()
     seismic_client.run()
 
-stream_thread = threading.Thread(target=stream_data_to_kafka, daemon=True)
-stream_thread.start()
+@app.on_event("startup")
+def start_seedlink_thread():
+    stream_thread = threading.Thread(target=stream_data_to_kafka, daemon=True)
+    stream_thread.start()
+    logger.info("Started SeedLink client thread 🚀")
