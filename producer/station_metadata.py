e830b1c8ca666f04815cd41be4b7d6a86b643c24
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-04-25 17:47:03 +0700

    chore: added stations

--- producer/station_metadata.py
+++ producer/station_metadata.py
@@ -2,4 +2,10 @@ STATIONS = [
     {"network": "CL", "station": "AGRP", "location": "", "channel": "HHZ"},
     {"network": "CL", "station": "AGRP", "location": "", "channel": "HHN"},
     {"network": "CL", "station": "AGRP", "location": "", "channel": "HHE"},
+    {"network": "CL", "station": "MALA", "location": "", "channel": "HHZ"},
+    {"network": "CL", "station": "MALA", "location": "", "channel": "HHN"},
+    {"network": "CL", "station": "MALA", "location": "", "channel": "HHE"},
+    {"network": "CL", "station": "TRIZ", "location": "", "channel": "HHZ"},
+    {"network": "CL", "station": "TRIZ", "location": "", "channel": "HHN"},
+    {"network": "CL", "station": "TRIZ", "location": "", "channel": "HHE"},
 ]
