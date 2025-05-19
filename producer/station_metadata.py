aaa5a5dbe52807202f958dfd16f09bf497ebfa5b
ernestbenedictus <m010d4ky1623@bangkit.academy>
2025-05-19 15:09:34 +0700

    chore: downsized station lists

--- producer/station_metadata.py
+++ producer/station_metadata.py
@@ -1,16 +1,8 @@
 STATIONS = [
+    
     {"network": "GE", "station": "TNTI", "location": "", "channel": "BHZ"},
     {"network": "GE", "station": "TNTI", "location": "", "channel": "BHN"},
     {"network": "GE", "station": "TNTI", "location": "", "channel": "BHE"},
-
-
-    {"network": "GE", "station": "TOLI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "TOLI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "TOLI", "location": "", "channel": "BHE"},
-
-    {"network": "GE", "station": "GENI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "GENI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "GENI", "location": "", "channel": "BHE"},
 
     {"network": "GE", "station": "PMBI", "location": "", "channel": "BHZ"},
     {"network": "GE", "station": "PMBI", "location": "", "channel": "BHN"},
@@ -24,75 +16,83 @@ STATIONS = [
     {"network": "GE", "station": "SOEI", "location": "", "channel": "BHN"},
     {"network": "GE", "station": "SOEI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "SANI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "SANI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "SANI", "location": "", "channel": "BHE"},
-
     {"network": "GE", "station": "MMRI", "location": "", "channel": "BHZ"},
     {"network": "GE", "station": "MMRI", "location": "", "channel": "BHN"},
     {"network": "GE", "station": "MMRI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "PMBT", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "PMBT", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "PMBT", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "FAKI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "FAKI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "FAKI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "TOLI2", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "TOLI2", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "TOLI2", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "PLAI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "PLAI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "PLAI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "BKNI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "BKNI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "BKNI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "MNAI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "MNAI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "MNAI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "UGM", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "UGM", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "UGM", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "SMRI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "SMRI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "SMRI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "FAKI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "FAKI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "FAKI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "LHMI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "LHMI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "LHMI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "CISI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "CISI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "CISI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "LUWI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "LUWI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "LUWI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "BNDI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "BNDI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "BNDI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "JAGI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "JAGI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "JAGI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "PLAI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "PLAI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "PLAI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "TOLI2", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "TOLI2", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "TOLI2", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "MNAI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "MNAI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "MNAI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "TOLI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "TOLI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "TOLI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "GSI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "GSI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "GSI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "GENI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "GENI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "GENI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "SMRI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "SMRI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "SMRI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "SANI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "SANI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "SANI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "SAUI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "SAUI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "SAUI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "PMBT", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "PMBT", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "PMBT", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "YOGI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "YOGI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "YOGI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "YOGI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "YOGI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "YOGI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "LHMI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "LHMI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "LHMI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "BKNI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "BKNI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "BKNI", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "LUWI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "LUWI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "LUWI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "UGM", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "UGM", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "UGM", "location": "", "channel": "BHE"},
 
-    {"network": "GE", "station": "JAGI", "location": "", "channel": "BHZ"},
-    {"network": "GE", "station": "JAGI", "location": "", "channel": "BHN"},
-    {"network": "GE", "station": "JAGI", "location": "", "channel": "BHE"},
+    # {"network": "GE", "station": "CISI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "CISI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "CISI", "location": "", "channel": "BHE"},
+
+    # {"network": "GE", "station": "BNDI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "BNDI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "BNDI", "location": "", "channel": "BHE"},
+
+    # {"network": "GE", "station": "GSI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "GSI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "GSI", "location": "", "channel": "BHE"},
+
+    # {"network": "GE", "station": "SAUI", "location": "", "channel": "BHZ"},
+    # {"network": "GE", "station": "SAUI", "location": "", "channel": "BHN"},
+    # {"network": "GE", "station": "SAUI", "location": "", "channel": "BHE"},
 ]
